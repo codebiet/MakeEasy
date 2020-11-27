73d22b9abc9f2e1d152f5202334e64b4e98cb263
Kulvir Pratap Singh <kulvir72510@gmail.com>
2020-11-27 09:12:26 +0530

    login and signup

--- Makeeasy/urls.py
+++ Makeeasy/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tmp/',include('tmp.urls'))
+    path('',include('tmp.urls')),
+    path('',include("django.contrib.auth.urls")),
 ]
--- tmp/models.py
+++ tmp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.forms import User
 
 # Create your models here.
+class Meta:
+    model = User
+    username = ("username",)
+
